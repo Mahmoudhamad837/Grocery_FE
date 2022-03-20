98a7479b1a61bb9d072afdb54d24d727f569fea6
mahmoud-hemida7 <mhemida722@gmail.com>
2022-03-20 16:48:36 +0200

    ffff

--- src/app/pages/orders/orders.module.ts
+++ src/app/pages/orders/orders.module.ts
@@ -4,16 +4,24 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+
 
 
 @NgModule({
   declarations: [
     OrdersComponent,
-    OrderDetailsComponent
+    OrderDetailsComponent,
+    
+    
   ],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    SharedModule
   ],
   exports:[
     OrdersComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,35 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import {StepsModule} from 'primeng/steps';
 import { NzSliderModule } from 'ng-zorro-antd/slider';
 import {InputMaskModule} from 'primeng/inputmask';
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {DialogModule} from 'primeng/dialog';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {InputTextModule} from 'primeng/inputtext';
 
 const modules = [
   NzTabsModule,
   NzIconModule,
   StepsModule,
   NzSliderModule,
-  InputMaskModule
+  InputMaskModule,
+  TableModule,
+  ToastModule,
+  CalendarModule,
+  SliderModule,
+  MultiSelectModule,
+  ContextMenuModule,
+  DialogModule,
+  ButtonModule,
+  DropdownModule,
+  ProgressBarModule,
+  InputTextModule
 ]
 
 @NgModule({
